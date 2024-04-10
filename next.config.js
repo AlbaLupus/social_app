557e8eea7ff6428623339980dd6d3ed422f13c01
Rohan Vaidya <rohanvaidya64@gmail.com>
2024-04-10 00:42:07 -0500

    ignore ts errors

--- next.config.js
+++ next.config.js
@@ -1,38 +1,33 @@
-/** @type {import('next').NextConfig} */
 const nextConfig = {
   typescript: {
     ignoreBuildErrors: true,
   },
-    experimental: {
-      serverActions: true,
-      serverComponentsExternalPackages: ["mongoose"],
-      //serverComponentsExternalPackages is important for rendering mongoose actions
-    },
-    eslint: {
-      // Warning: This allows production builds to successfully complete even if
-      // your project has ESLint errors.
-      ignoreDuringBuilds: true,
-    },
-    images: {
-      remotePatterns: [
-        {
-          protocol: "https",
-          hostname: "img.clerk.com",
-        },
-        {
-          protocol: "https",
-          hostname: "images.clerk.dev",
-        },
-        {
-          protocol: "https",
-          hostname: "uploadthing.com",
-        },
-        {
-          protocol: "https",
-          hostname: "placehold.co",
-        },
-      ],
-    },
-  };
-  
-  module.exports = nextConfig;
+  experimental: {
+    serverActions: true,
+    serverComponentsExternalPackages: ["mongoose"],
+  },
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+  images: {
+    remotePatterns: [
+      { protocol: "https", hostname: "img.clerk.com" },
+      { protocol: "https", hostname: "images.clerk.dev" },
+      { protocol: "https", hostname: "uploadthing.com" },
+      { protocol: "https", hostname: "placehold.co" },
+    ],
+  },
+  webpack: (config, { isServer }) => {
+    if (!isServer) {
+      // Here we manually add the 'encoding' module to the client-side bundle
+      config.resolve.fallback = { 
+        ...config.resolve.fallback,
+        // Add the encoding polyfill here
+        encoding: require.resolve('encoding')
+      };
+    }
+    return config;
+  },
+};
+
+module.exports = nextConfig;
